fe36f84192ad359ab9ab968e7a457ca70356a32d
Andrew Cunningham <old.noakes@gmail.com>
2015-04-21 02:04:54 +1000

    Migrated to platform api away from rest client, sorted out command to get correct return

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rest-client'
 gem 'highline'
+gem 'platform-api'
+gem 'rendezvous'
--- naive.rb
+++ naive.rb
@@ -1,27 +1,43 @@
 require 'highline/import'
-require 'rest-client'
-
-# Get auth token - have hardcoded for now with mine
-# Get all apps - again, have hardcoded to verify implementation
-# For each app
-#   Post to dynos under the app to run the command we are looking for (currently injecting values in)
-#   Attempt to attach to rendezvous endpoint to capture output
-#
-
-# curl -L -v https://api.heroku.com/apps/murmuring-refuge-8335/dynos -H "Authorization: Bearer " -H "Accept: application/vnd.heroku+json; version=3" -H "Content-Type: application/json" -X POST -d '{ "attach":true, "command":"rails runner \"puts app.remote_addr\"" }'
+require 'platform-api'
+require 'rendezvous'
 
 class HerokuInfoExtractor
 
   def initialize(auth_token)
-    @auth_token = auth_token
+    @heroku = PlatformAPI.connect_oauth(auth_token)
   end
 
-  def run_command
-    response = RestClient.get 'https://api.heroku.com/apps/murmuring-refuge-8335/dynos', { :Authorization => "Bearer #{@auth_token}", :Accept => 'application/vnd.heroku+json; version=3' }
-    puts response
+  def extract(appname_array)
+    apps_to_extract_from = appname_array.empty? ? get_all_apps : appname_array
+    apps_to_extract_from.inject({}) do |result, app|
+      result.merge(get_client_location_web_themes(app))
+    end
+  end
+
+  private
+
+  def extraction_command
+    'NEWRELIC_AGENT_ENABLED=false rails runner "puts({ Client.first.name => Location.all.inject({}) { |result, location| result.update(location.name => location.website.web_templates.all.find{ |temp| temp.web_theme }.web_theme.name) } }.to_json)" 2>/dev/null'
+  end
+
+  def get_client_location_web_themes(application_name)
+    response = @heroku.dyno.create(application_name, {'attach' => true, 'command' => extraction_command})
+    rz = Rendezvous.new({input:StringIO.new, output:StringIO.new, url: response['attach_url'], activity_timeout:300})
+    rz.start
+    JSON.parse(rz.output.string)
+  end
+
+  def get_all_apps
+    # This could be expensive if you have 100 apps on the account - could limit it
+    @heroku.app.list.map { |app| app["name"] }
   end
 end
 
-auth = ask( "Please provide your heroku bearer token: " ) { |auth_token| auth_token.echo = false }
-extractor = HerokuInfoExtractor.new(auth)
-extractor.run_command
+auth = ask( "Please provide your heroku oauth token: " ) { |auth_token| auth_token.echo = false }
+apps_input = ask( "Please provide apps in CSV format (empty will default to all apps available to oauth token): ")
+
+apps = apps_input.split(',').map(&:strip)
+
+extractor = HerokuInfoExtractor.new('ea33e132-4291-48ae-8e17-e58856cec4de')
+puts extractor.extract(apps)
